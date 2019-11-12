ec949988947452f8dff4d5ee7cf9e515f3832420
Calvin Beck <hobbes@ualberta.ca>
2019-11-12 18:55:21 -0500

    Start of StartTLS negotiations.

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@
 module Main where
 
 import Data.Conduit.Network
+import Data.Conduit.Network.TLS
 import Data.Conduit
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
@@ -18,55 +19,32 @@ import Data.Char
 xmppPort :: Int
 xmppPort = 5222
 
+xmppTlsPort :: Int
+xmppTlsPort = 5223
+
 settings :: ServerSettings
 settings = serverSettings xmppPort "*"
 
-handleClient :: AppData -> IO ()
-handleClient ad =
-  runConduit $ do
+startTLS :: AppData -> IO ()
+startTLS ad = runConduit $ do
   stream <- appSource ad .| parseBytes def .| awaitStream
   liftIO $ putStrLn "New connection"
   yield streamResp .| appSink ad
-  appSource ad .| parseBytes def .| awaitForever (lift . print)
+  liftIO $ putStrLn "Awaiting TLS"
+  starttls <- appSource ad .| parseBytes def .| awaitStartTls
+  liftIO $ putStrLn "Sending TLS proceed"
+  yield "<proceed xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>" .| appSink ad
+  liftIO $ putStrLn "Closing unencrypted channel."
+
+handleClient :: AppData -> IO ()
+handleClient ad = runConduit $ do
+  liftIO $ putStrLn "New TLS connection."
+  stream <- appSource ad .| parseBytes def .| awaitStream
   liftIO $ print stream
+  appSource ad .| parseBytes def .| awaitForever (lift . print)
+  liftIO $ putStrLn "wah"
 
 
-
-data Stream = MkStream { _streamVersion :: Text
-                       , _streamTo      :: Text
-                       , _streamId      :: Maybe Text
-                       }
-              deriving (Show, Eq)
-
--- parseStream :: MonadThrow m => ConduitT Event o m (Maybe [Text])
--- parseStream = tag' "stream" parseAttributes $ \(to, version) -> manyYield parseChats
---       where parseAttributes = (,) <$> requireAttr "to" <*> requireAttr "version" <* ignoreAttrs
-
-
-parseStream :: MonadThrow m => ConduitT Event Text m ()
-parseStream = force "blah" (tagIgnoreAttrs "stream" (manyYield parseChats))
-
-parseChats :: MonadThrow m => ConduitT Event o m (Maybe Text)
-parseChats = tagIgnoreAttrs "chat" content
-
-chatTest :: Text
-chatTest = "<chat>yay</chat>"
-
-xmlTestSmall :: Text
-xmlTestSmall = "<stream:stream version='1.0' to='blah2'><chat>yay</chat><chat>wooo</chat><stream:stream version='1.0' to='blah3'><chat>yay2</chat><chat>wooo2</chat></stream:stream><stream:stream version='1.0' to='blah2'><chat>yay</chat><chat>wooo</chat>"
-
-xmlTest :: Text
-xmlTest =
-  "<?xml version='1.0'?> \
-\      <stream:stream  \
-\          from='juliet@im.example.com'  \
-\          to='im.example.com'  \
-\          version='1.0'  \
-\          xml:lang='en'  \
-\          xmlns='jabber:client'  \
-\          xmlns:stream='http://etherx.jabber.org/streams'>" 
-
---streamResp :: ByteString
 -- TODO fix this. Generate id randomly.
 streamResp =
   "<?xml version='1.0'?> \
@@ -85,25 +63,6 @@ streamResp =
 \ </starttls> \
 \ </features>"
 
-
-test :: IO ()
-test = runConduit $ yield xmlTestSmall .| parseText def .| parseStream .| awaitForever (lift . print)
-
-streamBegin :: MonadThrow m => r -> ConduitT Event a m r -> ConduitT Event a m r
-streamBegin r rest = do
-  element <- await
-  case element of
-    Just e@(EventBeginElement n ats) -> do
-      if n == (Name {nameLocalName = "stream", nameNamespace = Just "http://etherx.jabber.org/streams", namePrefix = Just "stream"})
-        then rest >> streamBegin r rest
-        else streamBegin r rest
-    Just (EventEndElement n) -> do
-      if n == "stream"
-        then return r  -- Close stream with default value
-        else streamBegin r rest
-    Nothing -> return r
-    _ -> streamBegin r rest
-
 awaitStream :: MonadThrow m => ConduitT Event a m (Maybe Event)
 awaitStream = do
   element <- await
@@ -115,89 +74,22 @@ awaitStream = do
     Nothing -> return Nothing
     _ -> awaitStream
 
+awaitStartTls :: MonadThrow m => ConduitT Event a m (Maybe Event)
+awaitStartTls = do
+  element <- await
+  case element of
+    Just (EventBeginElement n ats) ->
+      if n == (Name {nameLocalName = "starttls", nameNamespace = Just "urn:ietf:params:xml:ns:xmpp-tls", namePrefix = Nothing})
+        then return element
+        else awaitStartTls
+    Nothing -> return Nothing
+    _ -> awaitStartTls
+
 main :: IO ()
-main = runTCPServer settings handleClient
-
-
--- -- | The most generic way to parse a tag. It takes a 'NameMatcher' to check whether
--- -- this is a correct tag name, an 'AttrParser' to handle attributes, and
--- -- then a parser to deal with content.
--- --
--- -- 'Events' are consumed if and only if the tag name and its attributes match.
--- --
--- -- This function automatically absorbs its balancing closing tag, and will
--- -- throw an exception if not all of the attributes or child elements are
--- -- consumed. If you want to allow extra attributes, see 'ignoreAttrs'.
--- --
--- -- This function automatically ignores comments, instructions and whitespace.
--- myTag :: MonadThrow m
---     => NameMatcher a -- ^ Check if this is a correct tag name
---                      --   and return a value that can be used to get an @AttrParser@.
---                      --   If this fails, the function will return @Nothing@
---     -> (a -> AttrParser b) -- ^ Given the value returned by the name checker, this function will
---                            --   be used to get an @AttrParser@ appropriate for the specific tag.
---                            --   If the @AttrParser@ fails, the function will also return @Nothing@
---     -> (b -> ConduitT Event o m c) -- ^ Handler function to handle the attributes and children
---                                    --   of a tag, given the value return from the @AttrParser@
---     -> ConduitT Event o m (Maybe c)
--- myTag nameMatcher attrParser f = do
---   (x, leftovers) <- dropWS []
---   res <- case x of
---     Just (EventBeginElement name as) -> case runNameMatcher nameMatcher name of
---       Just y -> case runAttrParser' (attrParser y) as of
---         Left _ -> return Nothing
---         Right z -> do
---           z' <- f z
---           (a, _leftovers') <- dropWS []
---           case a of
---             Just (EventEndElement name')
---               | name == name' -> return (Just z')
---             _ -> lift $ throwM $ InvalidEndElement name a
---       Nothing -> return Nothing
---     _ -> return Nothing
-
---   case res of
---     -- Did not parse, put back all of the leading whitespace events and the
---     -- final observed event generated by dropWS
---     Nothing -> Prelude.mapM_ leftover leftovers
---     -- Parse succeeded, discard all of those whitespace events and the
---     -- first parsed event
---     Just _  -> return ()
-
---   return res
---   where
---     -- Drop Events until we encounter a non-whitespace element. Return all of
---     -- the events consumed here (including the first non-whitespace event) so
---     -- that the calling function can treat them as leftovers if the parse fails
---     dropWS leftovers = do
---         x <- await
---         let leftovers' = maybe id (:) x leftovers
-
---         case isWhitespace <$> x of
---           Just True -> dropWS leftovers'
---           _         -> return (x, leftovers')
---     runAttrParser' p as =
---         case runAttrParser p as of
---             Left e           -> Left e
---             Right ([], x)    -> Right x
---             Right (attr', _) -> Left $ toException $ UnparsedAttributes attr'
-
--- -- | A simplified version of 'tag' where the 'NameMatcher' result isn't forwarded to the attributes parser.
--- --
--- -- Since 1.5.0
--- myTag' :: MonadThrow m
---      => NameMatcher a -> AttrParser b -> (b -> ConduitT Event o m c)
---      -> ConduitT Event o m (Maybe c)
--- myTag' a b = tag a (const b)
-
-
--- isWhitespace :: Event -> Bool
--- isWhitespace EventBeginDocument             = True
--- isWhitespace EventEndDocument               = True
--- isWhitespace EventBeginDoctype{}            = True
--- isWhitespace EventEndDoctype                = True
--- isWhitespace EventInstruction{}             = True
--- isWhitespace (EventContent (ContentText t)) = T.all isSpace t
--- isWhitespace EventComment{}                 = True
--- isWhitespace (EventCDATA t)                 = T.all isSpace t
--- isWhitespace _                              = False
+main = do
+--  forkTCPServer settings handleClient
+  runTCPServerStartTLS (tlsConfig "*" xmppPort "cert.pem" "key.pem") $
+    \(appData, stls) -> do
+      startTLS appData
+      liftIO $ putStrLn "Starting TLS..."
+      stls $ handleClient
