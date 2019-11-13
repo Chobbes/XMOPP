bbc69d8737705e0b3643e83e316f91475f07406d
Calvin Beck <hobbes@ualberta.ca>
2019-11-12 22:40:08 -0500

    Get authentication from SASL.

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,7 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Control.Monad.Catch
 import Data.Text (Text, unpack)
+import Data.Text.Encoding
 import qualified Data.Text as T
 import Data.XML.Types
 import Text.XML hiding (parseText)
@@ -15,6 +16,8 @@ import Text.XML.Stream.Parse
 import qualified Text.XML.Stream.Render as XR
 import Data.Conduit.List as CL
 import Data.Char
+import qualified Data.ByteString as BS
+import Data.ByteString.Base64 (decodeLenient)
 
 xmppPort :: Int
 xmppPort = 5222
@@ -42,6 +45,8 @@ handleClient ad = runConduit $ do
   liftIO $ putStrLn "New TLS connection."
   yield authResp .| appSink ad
   liftIO $ print stream
+  auth <- appSource ad .| parseBytes def .| awaitAuth
+  liftIO $ print auth
   appSource ad .| parseBytes def .| awaitForever (lift . print)
   liftIO $ putStrLn "wah"
 
@@ -82,32 +87,39 @@ authResp =
 \ </features>"
 
 awaitStream :: MonadThrow m => ConduitT Event a m (Maybe Event)
-awaitStream = do
-  element <- await
-  case element of
-    Just (EventBeginElement n ats) ->
-      if n == (Name {nameLocalName = "stream", nameNamespace = Just "http://etherx.jabber.org/streams", namePrefix = Just "stream"})
-        then return element
-        else awaitStream
-    Nothing -> return Nothing
-    _ -> awaitStream
+awaitStream = awaitName (Name {nameLocalName = "stream", nameNamespace = Just "http://etherx.jabber.org/streams", namePrefix = Just "stream"})
 
 awaitStartTls :: MonadThrow m => ConduitT Event a m (Maybe Event)
-awaitStartTls = do
+awaitStartTls = awaitName (Name {nameLocalName = "starttls", nameNamespace = Just "urn:ietf:params:xml:ns:xmpp-tls", namePrefix = Nothing})
+
+awaitName :: MonadThrow m => Name -> ConduitT Event a m (Maybe Event)
+awaitName name = do
   element <- await
   case element of
     Just (EventBeginElement n ats) ->
-      if n == (Name {nameLocalName = "starttls", nameNamespace = Just "urn:ietf:params:xml:ns:xmpp-tls", namePrefix = Nothing})
+      if n == name
         then return element
-        else awaitStartTls
+        else awaitName name
     Nothing -> return Nothing
-    _ -> awaitStartTls
+    _ -> awaitName name
+
+awaitAuth :: MonadThrow m => ConduitT Event a m (Maybe (Text, Text))
+awaitAuth = do
+  authStr <- tagIgnoreAttrs (matching (==(Name {nameLocalName = "auth", nameNamespace = Just "urn:ietf:params:xml:ns:xmpp-sasl", namePrefix = Nothing}))) content
+  return $ do
+    auth <- authStr
+    case decodeUtf8 <$> (BS.split 0 . decodeLenient $ encodeUtf8 auth) of
+      [_, user, pass] -> return (user, pass)
+      _               -> Nothing
+
+-- <auth xmlns="urn:ietf:params:xml:ns:xmpp-sasl" mechanism="PLAIN">AHRlc3QAZm9vYmFy</auth>
 
 main :: IO ()
-main = do
---  forkTCPServer settings handleClient
-  runTCPServerStartTLS (tlsConfig "*" xmppPort "cert.pem" "key.pem") $
-    \(appData, stls) -> do
-      startTLS appData
-      liftIO $ putStrLn "Starting TLS..."
-      stls $ handleClient
+main =
+  runTCPServerStartTLS (tlsConfig "*" xmppPort "cert.pem" "key.pem") xmpp
+
+xmpp :: (AppData, (AppData -> IO ()) -> IO a) -> IO a
+xmpp (appData, stls) = do
+  startTLS appData
+  liftIO $ putStrLn "Starting TLS..."
+  stls handleClient
