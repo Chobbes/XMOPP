d1838cb4e7e4ad9fd74b29b835bb5dcdf987a80b
Calvin Beck <hobbes@ualberta.ca>
2019-11-27 19:30:24 -0500

    Cleanup messages a bit.

---
+++ src/InternalMessaging.hs
@@ -0,0 +1,68 @@
+{- Messaging within the server -}
+module InternalMessaging where
+
+import Conduit
+import Data.Conduit.TMChan
+
+import GHC.Conc (atomically, forkIO, STM)
+import Control.Concurrent.STM.Map as STC
+
+import qualified Data.Text as T
+import Data.Text (Text, unpack)
+
+import Control.Monad
+import Data.Maybe
+
+import Text.XML hiding (parseText)
+
+import XMPP
+
+-- | Look up channels associated with a given jid in the channel map, and send
+-- an element over that channel.
+--
+-- If the jid has a resource as well, only send to that specific resource.
+sendToJid
+  :: MonadIO m =>
+     ChanMap -> Text -> Element -> m ()
+sendToJid cm to elem =
+  case T.splitOn "/" to of
+    [jid]           -> sendToJidAll cm jid elem
+    [jid, resource] -> sendToResource cm jid resource elem
+    (jid:_)         -> sendToJidAll cm jid elem
+    _               -> return ()
+
+-- | Look up channels associated with a given jid in the channel map, and send
+-- an element over that channel.
+sendToJidAll
+  :: MonadIO m =>
+     ChanMap -> Text -> Element -> m ()
+sendToJidAll cm to elem = do
+  channels <- liftIO . atomically $ do
+    mm <- STC.lookup to cm
+    case mm of
+      Nothing      -> return []
+      Just (rs, m) -> forM rs $ \r -> do
+        maybeChan <- STC.lookup r m
+        return $ maybeToList maybeChan
+  liftIO $ putStrLn $ "Message to: " ++ show to
+  liftIO $ print $ length channels
+  writeToAllChannels (Prelude.concat channels) elem
+
+-- | Send message to a *specific* XMPP resource.
+sendToResource :: MonadIO m =>
+     ChanMap -> Text -> Text -> Element -> m ()
+sendToResource cm jid resource elem = do
+  mchan <- liftIO . atomically $ do
+    mm <- STC.lookup jid cm
+    case mm of
+      Nothing     -> return Nothing
+      Just (_, m) -> STC.lookup resource m
+  case mchan of
+    Nothing   -> return ()
+    Just chan -> liftIO . atomically $ writeTMChan chan elem
+
+-- | Write to a value to each channel in a list.
+writeToAllChannels
+  :: MonadIO m => [TMChan a] -> a -> m ()
+writeToAllChannels channels elem =
+  forM_ channels $ \chan -> liftIO . atomically $ writeTMChan chan elem
--- src/Messages.hs
+++ src/Messages.hs
@@ -3,27 +3,17 @@
 module Messages where
 
 import Conduit
-import Data.Conduit.TMChan
 
-import Control.Monad.Catch
-import Control.Monad.IO.Class
-
-import GHC.Conc (atomically, forkIO, STM)
-import Control.Concurrent.STM.Map as STC
-
-import qualified Data.Text as T
 import Data.Text (Text, unpack)
 
 import qualified Data.Map as M
-import Control.Monad
-import Data.Maybe
 
 import Text.XML hiding (parseText)
 import Text.XML.Stream.Parse
 import Data.XML.Types (Event(..), Content(..))
-import qualified Text.XML.Stream.Render as XR
 
 import XMPP
+import InternalMessaging
 
 -- | Conduit to receive messages, and then forward them to a message handler.
 receiveMessage
@@ -64,53 +54,3 @@ messageBody to from i ty = do
         ]
 
   return (mkElem <$> body <*> threadId)
-
--- | Look up channels associated with a given jid in the channel map, and send
--- an element over that channel.
---
--- If the jid has a resource as well, only send to that specific resource.
-sendToJid
-  :: MonadIO m =>
-     ChanMap -> Text -> Element -> m ()
-sendToJid cm to elem =
-  case T.splitOn "/" to of
-    [jid]           -> sendToJidAll cm jid elem
-    [jid, resource] -> sendToResource cm jid resource elem
-    (jid:_)         -> sendToJidAll cm jid elem
-    _               -> return ()
-
--- | Look up channels associated with a given jid in the channel map, and send
--- an element over that channel.
-sendToJidAll
-  :: MonadIO m =>
-     ChanMap -> Text -> Element -> m ()
-sendToJidAll cm to elem = do
-  channels <- liftIO . atomically $ do
-    mm <- STC.lookup to cm
-    case mm of
-      Nothing      -> return []
-      Just (rs, m) -> forM rs $ \r -> do
-        maybeChan <- STC.lookup r m
-        return $ maybeToList maybeChan
-  liftIO $ putStrLn $ "Message to: " ++ show to
-  liftIO $ print $ length channels
-  writeToAllChannels (Prelude.concat channels) elem
-
--- | Send message to a *specific* XMPP resource.
-sendToResource :: MonadIO m =>
-     ChanMap -> Text -> Text -> Element -> m ()
-sendToResource cm jid resource elem = do
-  mchan <- liftIO . atomically $ do
-    mm <- STC.lookup jid cm
-    case mm of
-      Nothing     -> return Nothing
-      Just (_, m) -> STC.lookup resource m
-  case mchan of
-    Nothing   -> return ()
-    Just chan -> liftIO . atomically $ writeTMChan chan elem
-
--- | Write to a value to each channel in a list.
-writeToAllChannels
-  :: MonadIO m => [TMChan a] -> a -> m ()
-writeToAllChannels channels elem =
-  forM_ channels $ \chan -> liftIO . atomically $ writeTMChan chan elem
